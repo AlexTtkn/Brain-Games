ecf8106981942fc42d32dd9d9663d14f133dd9c6
Alex <alextetekin@gmail.com>
2023-05-23 13:30:25 +0300

    Add Games class, changed App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetingsClient();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        switch (scanner.nextLine()) {
+            case "1" -> {
+                System.out.println("Your choice: 1\n");
+                Cli.greetingsClient();
+            }
+            case "2" -> {
+                System.out.println("Your choice: 2\n");
+                Games.evenChoice();
+            }
+            case "0" -> {
+                System.out.println("Exit");
+            }
+            default -> {
+                System.out.println();
+            }
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greetingsClient() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.nextLine();
         System.out.printf("Hello, %s! \n", name);
