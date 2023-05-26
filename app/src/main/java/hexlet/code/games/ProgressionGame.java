d04a4ac69b407da60a33a589ae10f9fc99d6e278
Alex <alextetekin@gmail.com>
2023-05-26 18:54:30 +0300

    fix: change ProgressionGame

--- app/src/main/java/hexlet/code/games/ProgressionGame.java
+++ app/src/main/java/hexlet/code/games/ProgressionGame.java
@@ -2,30 +2,41 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class ProgressionGame {
+    private static final String[] MATRIX = new String[10];
+
     public static void progressionChoice() {
         String clientName = Engine.greetingsClient();
         System.out.println("What number is missing in the progression?");
         for (int i = 0; i < 3; i++) {
-            String[] matrix = new String[10];
             int randomNumber1 = new Random().nextInt(10);
             int randomNumber2 = new Random().nextInt(10);
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[j] = String.valueOf(randomNumber1);
+            for (int j = 0; j < MATRIX.length; j++) {
+                MATRIX[j] = String.valueOf(randomNumber1);
                 randomNumber1 += randomNumber2;
             }
             int pointsPosition = new Random().nextInt(10);
             String points = "..";
-            String correctAnswer = matrix[pointsPosition];
-            matrix[pointsPosition] = points;
-            System.out.println("Question: " + Arrays.toString(matrix));
+            String correctAnswer = MATRIX[pointsPosition];
+            MATRIX[pointsPosition] = points;
+            System.out.print("Question: ");
+            printNumbers();
             String clientAnswer = Engine.checkCorrectAnswer(correctAnswer, clientName);
             if (!clientAnswer.equals(correctAnswer)) {
                 Engine.wrongAnswer(clientAnswer, correctAnswer, clientName);
                 break;
+            }
+        }
+    }
+
+    private static void printNumbers() {
+        for (int i = 0; i < MATRIX.length; i++) {
+            if (i == MATRIX.length - 1) {
+                System.out.println(MATRIX[i]);
+            } else {
+                System.out.print(MATRIX[i] + ", ");
             }
         }
     }
